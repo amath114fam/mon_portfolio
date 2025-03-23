f2fbf95fa0735c0a76a0464d74730be6791a1b18
amath114fam <afam26641@gmail.com>
2025-03-23 15:12:53 +0000

    douzième commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+  document.addEventListener( 'DOMContentLoaded', function() {
+    var splide = new Splide( '.splide',{
+        type:'loop',
+        autoplay:true,
+        interval:4000
+    });
+    splide.mount();
+  } );
+  const ul =document.querySelector(".nav ul");
+  const burger=document.getElementById("burger");
+  burger.addEventListener("click",()=>{
+    const trash=document.createElement("li");
+    trash.innerHTML='<i class="fa-solid fa-xmark"></i>';
+    trash.classList.add("xmark");
+    ul.appendChild(trash);
+    // ul.classList.add("nave");
+    ul.style.display="block"
+    burger.style.display="none"
+    if (trash) {
+      trash.addEventListener("click",()=>{
+        ul.style.display="none"
+       burger.style.display="block"
+      })
+    }
+  })
+  
