70185c8458e566d6ca97ef5e72f618c633a0c8ab
amath114fam <afam26641@gmail.com>
2025-03-30 02:35:30 +0000

    premier commit

--- index.js
+++ index.js
@@ -24,6 +24,36 @@
       })
     }
   })
+  const btn=document.getElementById("btn");
+  // btn.addEventListener("click",()=>{
+  //   alert("no")
+  // })
+  const nom=document.getElementById("nom")
+  const prenom=document.getElementById("prenom")
+  const emaile=document.getElementById("mail")
+  const messe=document.getElementById("texter")
+  const ob=document.getElementById("object")
+  const tell=document.getElementById("tel")
+  
+  btn.addEventListener("click",()=>{
+    let nam=document.getElementById("nom").value
+    let maile=document.getElementById("mail").value
+    let mess=document.getElementById("texter").value
+    const nomvide=nam.trim()===""
+    const mailvide=maile.trim()===""
+    const messvide=mess.trim()===""
+    if (nomvide||mailvide||messvide) {
+      alert("non");
+    } else {
+      sendMail();
+    }
+    nom.value=""
+    prenom.value=""
+    emaile.value=""
+    messe.value=""
+    ob.value=""
+    tell.value=""
+  })
   
   function sendMail(){
     let params={
@@ -34,6 +64,7 @@
       message:document.getElementById("texter").value,
       phone:document.getElementById("tel").value,
     }
-    emailjs.send("service_x58lvu9","template_oia6h6z",params).then(alert("Message envoyé avec succes"));
+    emailjs.send("service_4hqc0ti","template_7yaw37s",params).then(alert("Message envoyé avec succès"));
   }
+
   
